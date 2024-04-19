4b1df38eac37d876d078a219d32d7dbebc50a0b0
zeyad mohammed nagi <zeyadnagi421@gmail.com>
2024-04-19 21:16:26 +0200

    "Update Feed component, add icons, and update Navbar & index exports"
    AND
    
    This commit includes the following changes:
    
    1. The `Feed` component has been updated to include a new design with a sidebar and video

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,7 +1,50 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import { Box, Stack, Typography } from "@mui/material";
 
-const Feed = () => (
-    <div>Feed</div>
-  )
+import { fetchFromAPI } from "../utils/fetchFromAPI";
+import { SideBar, Videos } from "./";
 
-export default Feed
+const Feed = () => {
+  const [selectedCategory, setselectedCategory] = useState("New");
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`).then((data) =>
+      setVideos(data.items)
+    );
+  }, [selectedCategory]);
+
+  return (
+    <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
+      <Box
+        sx={{
+          height: { sx: "auto", md: "92vh" },
+          borderRight: "1px solid #3d3d3d",
+          px: { sx: 0, md: "2" },
+        }}
+      >
+        <SideBar
+          selectedCategory={selectedCategory}
+          setselectedCategory={setselectedCategory}
+        />
+
+        <Typography variant="body2" sx={{ mt: 1.5, color: "#fff" }}>
+          Copyright 2024
+        </Typography>
+      </Box>
+      <Box p={2} sx={{ overflowY: "auto", height: "92vh", flex: 2 }}>
+        <Typography
+          variant="h4"
+          fontWeight="bold"
+          mb={2}
+          sx={{ color: "white" }}
+        >
+          {selectedCategory} <span style={{ color: "#F31503" }}>Video</span>
+        </Typography>
+        <Videos videos={videos} />
+      </Box>
+    </Stack>
+  );
+};
+
+export default Feed;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,13 +10,22 @@ const Navbar = () => (
     alignItems="center"
     p={2}
     sx={{
+      zIndex : 999,
       justifyContent: "space-between",
       position: "sticky",
       top: 0,
       background: "#000",
     }}
   >
-    <Link to="/" style={{ display: "flex", alignItems: "center" }}>
+    <Link
+      to="/"
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-between",
+        width: "100%",
+      }}
+    >
       <img src={logo.src} alt={logo.alt} height={48} />
       <SearchBar />
     </Link>
